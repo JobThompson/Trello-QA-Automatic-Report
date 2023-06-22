cbd0bb9760fda73c407731019c8e2b26f4f71e27
JobThompson <jobt3483@gmail.com>
2023-06-22 11:30:06 -0400

    deleted existing

--- Trello_Project Reports GUI System/TrelloList.py
+++
@@ -1,7 +0,0 @@
-class TrelloList:
-    def __init__(self, list_info, card_data):
-        self.name = list_info['name']
-        self.id = list_info['id']
-        self.board_id = list_info['idBoard']
-        self.cards = card_data
-        self.card_objects = []
